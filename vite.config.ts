cc8ee7584c3c72e1ed76165447d74a965f5b17f1
Rebeca <elenrebeca280@gmail.com>
2025-12-16 19:37:52 -0300

    Update Pages base path after repo rename

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/meu_portf-lio/',
+  base: '/meu_portfolio/',
   plugins: [react()],
 });
